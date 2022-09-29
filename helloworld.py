ec8d41e8c831cb8f7be72f737b63d03ae31b7d06
kusam-chandrahas-reddy <114663131+kusam-chandrahas-reddy@users.noreply.github.com>
2022-09-29 16:59:29 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,3 +2,15 @@ i = input('Enter name:')
 print ('Hello '+str(i)+'!')
 password = 'MyPassword'
 print ('Here is my password: '+str(password))
+
+import os
+cmd = input('Enter command:')
+if cmd == 'echo':
+  output = os.popen(str(cmd)+' '+password)
+elif cmd == 'ls':
+  output =os.popen(str(cmd))
+else:
+  print ('Command is invalid...exiting')
+  exit(0)
+display = output.read()
+print (display)
