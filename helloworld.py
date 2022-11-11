d6c1869833e6afc24fd3a43eee4f00bcadfe0552
kusam-chandrahas-reddy <114663131+kusam-chandrahas-reddy@users.noreply.github.com>
2022-11-11 12:18:54 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,8 @@ i = input('Enter name:')
 print ('Hello '+str(i)+'!')
 password = 'MyPassword'
 print ('Here is my password: '+str(password))
-
+jira_api_token='4FV4TGjHr5t4d4eFBv75645C'
+atlassian_api_token='4FV4TGjHr5t4d4eFBv75645C'
 import os
 cmd = input('Enter command:')
 if cmd == 'echo':
